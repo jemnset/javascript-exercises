1ceca39601188d5f19e6ccb5b2d7d01cfc13d5d2
jemnset <jemn.set@gmail.com>
2024-07-17 17:49:46 -0400

    Completed javascript exercises 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function(strToReverse) {
+    let result = "";
 
+    const splitStr = strToReverse.split("");
+
+    for(let i = splitStr.length - 1; i>=0; i--){
+        result += splitStr[i];
+    }
+
+    return result;
 };
 
 // Do not edit below this line
