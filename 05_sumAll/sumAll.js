6babf2cfaa889b6e1f7f3d15217c4bd8dedf0a5f
jemnset <jemn.set@gmail.com>
2024-07-18 11:21:35 -0400

    Completed javascript exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
 
+    let result = 0;
+
+    if(isNaN(min) || isNaN(max) 
+        || !Number.isInteger(min) || !Number.isInteger(max) 
+        || min < 0 || max < 0)
+        return "ERROR";
+
+    if(min > max){
+        let temp = min;
+        min = max;
+        max = temp;
+    }
+
+    for(let i=min; i<=max; i++){
+        result+=i;
+    }
+
+    return result;
 };
 
 // Do not edit below this line
