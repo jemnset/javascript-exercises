4d1ecbd9840993a8c6ff0745ef7e0105214f579e
jemnset <jemn.set@gmail.com>
2024-07-18 11:01:37 -0400

    Completed javascript exercises 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(arrayToRemoveFrom, ...argsToRemove) {
+
+    for(let i=0; i<arrayToRemoveFrom.length; i++){
+        for(arg of argsToRemove){
+            if(arrayToRemoveFrom[i] === arg){
+                arrayToRemoveFrom.splice(i, 1);
+                i--;
+                break;
+            }
+        }
+    }
+
+    return arrayToRemoveFrom;
 };
 
 // Do not edit below this line
