86d30682c90bb2f92684b90c1921155a5f2f2b85
jemnset <jemn.set@gmail.com>
2024-07-24 18:11:35 -0400

    Completed javascript exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((total,item) => total + item, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((total, item) => total * item);
 };
 
-const power = function() {
+const power = function(a, b) {
 	
+  /* let result = a;
+  for(let i=0; i<b; i++)
+    result *= a;
+
+  return result; */
+
+  return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if(num === 0 || num === 1)
+    return 1;
+
+  let result = num;
+  for(let i=num-1; i>0; i--)
+    result *= i;
+
+  return result;
 };
 
 // Do not edit below this line
