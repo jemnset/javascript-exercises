5c89315619c430a606be3d1c0fe2098a9865d06a
jemnset <jemn.set@gmail.com>
2024-07-24 23:31:06 -0400

    Completed javascript exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    const num = typeof n != "number" ? parseInt(n) : n;
 
+    if(num < 0) return 'OOPS';
+    if(num === 0) return 0;
+    //if(num === 1 || num === 2) return 1;
+
+    let prevNum = 1;
+    let currNum = 1;
+
+    for(let i=2; i<num; i++){
+        let total = prevNum + currNum;
+        prevNum = currNum;
+        currNum = total;
+    }
+
+    return currNum;
 };
 
 // Do not edit below this line
