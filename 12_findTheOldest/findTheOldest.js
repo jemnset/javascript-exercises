216ffc6fc106c9000345e3a684e1965292f50afa
jemnset <jemn.set@gmail.com>
2024-07-25 01:11:19 -0400

    Completed javascript exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const currentYear = (new Date()).getFullYear();
 
+    const ages = people.map((person) => (
+        {
+            name: person.name,
+            age: 'yearOfDeath' in person ? person.yearOfDeath - person.yearOfBirth : currentYear - person.yearOfBirth
+        }
+    ));
+
+    const oldest = ages.sort((a,b) => a.age > b.age ? -1 : 1);
+
+    return people.find((person) => person.name === oldest[0].name);
 };
 
 // Do not edit below this line
