89a16a86abf4851640a42c83c4e0797d79fcd73f
jemnset <jemn.set@gmail.com>
2024-07-24 23:11:28 -0400

    Completed javascript exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    const formattedWord = word
+        .replace(/[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g, '')
+        .split(' ')
+        .join('')
+        .toLowerCase();
 
+    const reversedWord = formattedWord.split('').reverse().join('');
+
+    return formattedWord === reversedWord;
 };
 
 // Do not edit below this line
